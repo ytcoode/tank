c9afff639a1fe70b0d2dc8951dc45cc8a786d28d
wangyuntao <wyt.daily@gmail.com>
2020-06-27 16:15:14 +0800

    To be continued

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,47 @@
-//! The simplest possible example that does something.
-
 use ggez;
 use ggez::event;
+use ggez::event::EventHandler;
 use ggez::graphics;
-use ggez::nalgebra as na;
-use ggez::{Context, GameResult};
+use ggez::nalgebra;
+use ggez::Context;
+use ggez::GameResult;
 
-struct MainState {
+struct GameState {
     pos_x: f32,
 }
 
-impl MainState {
-    fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+impl GameState {
+    fn new() -> GameResult<GameState> {
+        let s = GameState { pos_x: 0.0 };
         Ok(s)
     }
 }
 
-impl event::EventHandler for MainState {
+impl EventHandler for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+        graphics::clear(ctx, graphics::WHITE);
 
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            na::Point2::new(0.0, 0.0),
+            nalgebra::Point2::new(0.0, 0.0),
             100.0,
-            2.0,
-            graphics::WHITE,
+            0.1,
+            graphics::BLACK,
         )?;
-        graphics::draw(ctx, &circle, (na::Point2::new(self.pos_x, 380.0),))?;
+        graphics::draw(ctx, &circle, (nalgebra::Point2::new(self.pos_x, 380.0),))?;
 
-        graphics::present(ctx)?;
-        Ok(())
+        graphics::present(ctx)
     }
 }
 
 pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
-    let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut MainState::new()?;
-    event::run(ctx, event_loop, state)
+    let (mut ctx, mut event_loop) = ggez::ContextBuilder::new("super_simple", "ggez").build()?;
+    let mut state = GameState::new()?;
+    event::run(&mut ctx, &mut event_loop, &mut state)
 }
