16521bf2086b2ce0b69c168b2a9309da2f13fe47
wangyuntao <wyt.daily@gmail.com>
2020-06-27 21:50:59 +0800

    To be continued

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use ggez::event;
 use ggez::event::EventHandler;
 use ggez::graphics;
 use ggez::graphics::spritebatch::SpriteBatch;
-use ggez::graphics::Color;
+use ggez::graphics::DrawParam;
+use ggez::graphics::Image;
 use ggez::nalgebra::Point2;
+use ggez::nalgebra::Vector2;
 use ggez::Context;
 use ggez::ContextBuilder;
 use ggez::GameResult;
@@ -22,19 +24,39 @@ impl Tank {
 }
 
 struct GameState {
+    tile_width: f32,
+    tile_height: f32,
+    tile_batch: SpriteBatch,
+
     tanks: Vec<Tank>,
     tank_batch: SpriteBatch,
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> GameResult<GameState> {
-        let mut tanks = Vec::new();
-        let tank_texture = graphics::Image::new(ctx, "/PNG/Tanks/tankGreen.png")?;
-        let tank_batch = SpriteBatch::new(tank_texture);
+        let tile_texture = Image::new(ctx, "/PNG/Environment/sand.png")?;
+        let tile_width = tile_texture.width() as f32;
+        let tile_height = tile_texture.height() as f32;
+        let tile_batch = SpriteBatch::new(tile_texture);
 
+        let mut tanks = Vec::new();
         tanks.push(Tank::new());
 
-        Ok(GameState { tanks, tank_batch })
+        let tank_texture = Image::new(ctx, "/PNG/Tanks/tankGreen.png")?;
+        let tank_batch = SpriteBatch::new(tank_texture);
+
+        Ok(GameState {
+            tile_width,
+            tile_height,
+            tile_batch,
+            tanks,
+            tank_batch,
+        })
+    }
+
+    fn clear(&mut self) {
+        self.tile_batch.clear();
+        self.tank_batch.clear();
     }
 }
 
@@ -44,14 +66,36 @@ impl EventHandler for GameState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, Color::from((0.392, 0.584, 0.929)));
+        // clear
+        graphics::clear(ctx, graphics::WHITE);
 
-        self.tank_batch.clear();
+        // tile
+        let (w, h) = graphics::drawable_size(ctx);
+        let mut x = 0.0;
+        while x < w {
+            let mut y = 0.0;
+            while y < h {
+                self.tile_batch.add((Point2::new(x, y),));
+                y += self.tile_height;
+            }
+            x += self.tile_width;
+        }
+        graphics::draw(ctx, &self.tile_batch, (Point2::new(0.0, 0.0),))?;
+
+        // tank
         for tank in &self.tanks {
             self.tank_batch.add((tank.position,));
         }
-        graphics::draw(ctx, &self.tank_batch, (Point2::new(0.0, 0.0),))?;
+        graphics::draw(
+            ctx,
+            &self.tank_batch,
+            DrawParam::new()
+                .dest(Point2::new(0.0, 0.0))
+                .scale(Vector2::new(0.5, 0.5)),
+        )?;
 
+        // present
+        self.clear();
         graphics::present(ctx)
     }
 }
