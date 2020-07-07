95b4e2848604fbd35c504f47c7b297c501a5302f
wangyuntao <wyt.daily@gmail.com>
2020-07-07 12:43:08 +0800

    To be continued

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ mod map;
 mod position;
 mod tank;
 
-pub mod deps; // The 'pub' keyword is to suppress unused warnings
-pub mod util; // Same as above
+mod deps; // The 'pub' keyword is to suppress the dead code warnings
+mod util; // Same as above
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (mut ctx, mut event_loop) = ContextBuilder::new("CrazyTank", "CrazyBunny")
